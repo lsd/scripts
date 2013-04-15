6492258a97c4a91ba16c097573c1d92ea15b4fbe
lsd <zenchloride@gmail.com>
2013-04-15 01:16:23 -0400

    Updated cleanphp.rb to use perl instead of sed

---
+++ cleanphp-infected-example.php
@@ -0,0 +1,16 @@
+<?php 
+if ($x == 1) {
+   echo "yo";
+}
+
+// 
+1 2 3 4
+
+?>
+
+<?php if (true) { $x=1;} ?>
+<?php if ($x==1) {$x=1;} ?>
+
+
+<?php // This  Must  be readable! eval(base64_decode("and this!"));
+?>
--- cleanphp.rb
+++ cleanphp.rb
@@ -4,28 +4,41 @@
 # Make backups before running this!
 
 infected = 'dirty_files.txt'
+oxyclean = 'perl -pi -e \'s/eval\(base64_decode\("D.*?"\)\);//g\''
 
 unless File.exists? infected
-  p "NOT FOUND - All infected files should be in #{infected}"
+  puts "NOT FOUND - All infected files should be in #{infected}"
   exit
 end
 
-p "Starting..."
+puts "\nStarting (cleanser is #{oxyclean} \"file.php\")\n\n"
 
-total = 0
+bytes = 0
+cleaned = 0
 
 IO.foreach(infected) do |f|
   f.chop!
   before = File.size(f)
 
-  %x(sed -i '' 's/eval(base64_decode("D.*"));//g' #{f})
+  # skip presumably clean files
+  puts "* File < 601 bytes - skipping #{f}" and next unless before > 600
+
+  # backup file.php to file.dirty
+  puts "* Backing #{f} to #{f}.dirty"
+  %x(cp #{f} #{f}.dirty)
+
+  # dont use sed (need to be non-greedy) and be careful between *nix/os x distributions)
+  %x(#{oxyclean} "#{f}")
 
   diff = before - File.size(f)
-  p "#{diff} bytes removed from #{f}"
+  puts "* #{diff} bytes removed from #{f}\n\n" if diff > 0
 
-  p %x(php -l #{f})
+  # Check PHP syntax (lint)
+  #p %x(php -l #{f})
 
-  total += diff
+  cleaned += 1
+  bytes += diff
 end
 
-p "done. In total: (#{total}) bytes removed"
+puts "\n----------------------------\n"
+puts "Done. Stats: #{cleaned} files cleaned. #{bytes} bytes removed\n"
