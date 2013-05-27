9e23212dd27706305c3430218c04223bbdc68518
Isam <isam@withpulp.com>
2013-05-27 07:33:17 -0400

    Added defaults.rb

---
+++ defaults.rb
@@ -0,0 +1,16 @@
+#
+# Keep a backup (undo tree) and validate writes/reads to `defaults`
+#
+# For example, this will turn high-contrast most ON for iTunes
+# $ defaults write com.apple.iTunes high-contrast-mode-enable -bool TRUE<enter>
+
+
+
+"
+defaults
+write 
+com.apple.iTunes 
+high-contrast-mode-enable 
+-bool 
+TRUE
+"
