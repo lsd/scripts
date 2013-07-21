57416604241bc739fcf972570407d347ff48695a
Isam <isam@withpulp.com>
2013-07-21 02:13:38 -0400

    Open new Chrome process for dev (no form re-post warning)

---
+++ open-developer-chrome-process.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Isam github.com/lsd
+
+DATA_DIR='~/temp/Chrome'
+CHROME='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+SWITCHES="\
+  --user-data-dir='$DATA_DIR' \
+  --disable-prompt-on-repost \
+  --disable-popup-blocking \
+  --keep-alive-for-test \
+  --disable-extensions \
+  --no-default-browser-check \
+  --no-first-run \
+  --disable-login-animations \
+  --disable-geolocation \
+  --disable-translate \
+  --disable-webgl \
+  --disable-audio \
+  --disable-webaudio \
+  --show-icons \
+  --disable-plugins \
+  --disable-sync \
+  --show-icons \
+  --enable-file-cookies \
+  --no-pings \
+  --no-swap-metrics \
+  --disable-full-history-sync\
+"
+
+echo "Opening clean Google Chrome\n"
+echo "* chrome binary: $CHROME"
+echo "* user-data-dir: $DATA_DIR\n"
+echo "* args/switches:"
+for switch in $SWITCHES; do echo $switch; done
+echo "\n* More switches @ http://peter.sh/experiments/chromium-command-line-switches/\n"
+
+#echo "Proceed? (hit the Any key.)" && read
+# mkdir -p $DATA_DIR
+
+"$CHROME" 2> /dev/null $SWITCHES
+echo "Chrome returned $?"
